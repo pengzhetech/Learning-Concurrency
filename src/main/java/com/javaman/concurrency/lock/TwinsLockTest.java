96e8ac23d8cc49f6d1ebed03e18247d75d282041
pengzhe <pengzhe@rayvision.com>
2017-12-05 23:05:33 +0800

    自定义锁的实现

---
+++ src/main/java/com/javaman/concurrency/lock/TwinsLockTest.java
@@ -0,0 +1,46 @@
+package com.javaman.concurrency.lock;
+
+
+import com.javaman.concurrency.threads.SleepUtils;
+import org.junit.Test;
+
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @author:彭哲
+ * @Date:2017/12/5
+ */
+public class TwinsLockTest {
+    @Test
+    public void test() {
+        final Lock lock = new TwinsLock();
+        class Worker extends Thread {
+            @Override
+            public void run() {
+                while (true) {
+                    lock.lock();
+                    try {
+                        SleepUtils.second(1);
+                        System.out.println(Thread.currentThread().getName());
+                        SleepUtils.second(1);
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        }
+
+        //启动10个线程
+        for (int i = 0; i < 10; i++) {
+            Worker worker = new Worker();
+            worker.setDaemon(true);
+            worker.start();
+        }
+        //每隔一秒换行
+        for (int i = 0; i < 10; i++) {
+            SleepUtils.second(1);
+            System.out.println();
+        }
+
+    }
+}
